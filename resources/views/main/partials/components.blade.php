12b2764783e92d2f7fb1d0e91b639146b58fe8be
goldarizki <goldarizki@gmail.com>
2024-05-27 22:06:47 +0700

    Update Interface
    Progress:
     - Menambah graph
     - Merapikan codingan biar lebih modular
    
    Notes:
     - Selesaikan dan fix kan badges nilai yang ada di sidebar, serta tambahkan
       selector biar update lilainya gampang
     - Kalau sudah pastikan bisa fetch API dari server.

--- resources/views/main/partials/components.blade.php
+++ resources/views/main/partials/components.blade.php
@@ -1,22 +1,11 @@
 <script>
 
 
-
-
-var element = document.getElementById('chart_tegangan');
-
-
-var height = parseInt(KTUtil.css(element, 'height'));
-var labelColor = KTUtil.getCssVariableValue('--bs-gray-500');
-var borderColor = KTUtil.getCssVariableValue('--bs-gray-200');
-var baseColor = KTUtil.getCssVariableValue('--bs-warning');
-var lightColor = '#F8F5FF';
-
-var options = {
-    series: [{
-        name: 'Tegangan',
-        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
-    }],
+function buatChart(element, labelColor, borderColor, baseColor, sumbu_x, sumbu_y) {
+    let lightColor = '#F8F5FF';
+    let height = parseInt(KTUtil.css(element, 'height'));
+    let options = {
+    series: [sumbu_x],
     chart: {
         fontFamily: 'inherit',
         type: 'area',
@@ -44,7 +33,7 @@ var options = {
         colors: [baseColor]
     },
     xaxis: {
-        categories: ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02'],
+        categories: sumbu_y,
         axisBorder: {
             show: false,
         },
@@ -127,10 +116,191 @@ var options = {
         strokeColor: baseColor,
         strokeWidth: 3
     }
+    
 }
 
-var chart = new ApexCharts(element, options);
+let chart = new ApexCharts(element, options);
 
 chart.render();
+
+    return chart;
+}
+
+
+
+var chart_tegangan = buatChart(document.getElementById('chart_tegangan'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-info'), 
+    {
+        name: 'Tegangan',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_arus'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-danger'), 
+    {
+        name: 'Arus',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_faktor_daya'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-success'), 
+    {
+        name: 'Faktor Daya',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_frekuensi'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-primary'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_daya_aktif'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-orange'), 
+    {
+        name: 'Daya Aktif',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_daya_reaktif'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-success'), 
+    {
+        name: 'Daya Reaktif',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_daya_semu'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-teal'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_total_daya_aktif'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-orange'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_total_daya_reaktif'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-success'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_total_daya_semu'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-teal'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_total_faktor_daya'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-warning'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('daya_aktif_yang_diminta'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-orange'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+buatChart(document.getElementById('chart_daya_reaktif_yang_diminta'),
+    KTUtil.getCssVariableValue('--bs-gray-500'),
+    KTUtil.getCssVariableValue('--bs-gray-200'), 
+    KTUtil.getCssVariableValue('--bs-success'), 
+    {
+        name: 'Frekuensi',
+        data: [380, 385, 386, 384.6, 398.4, 385.2, 380.7]
+    }, 
+    ['Feb 23 08:56', 'Feb 23 08:57', 'Feb 23 08:58', 'Feb 23 08:59', 'Feb 23 09:00', 'Feb 23 09:01', 'Feb 23 09:02']
+);
+
+//    x:[387, 384, 386.2, 382.6, 390.5, 375.2, 378.7],
+        //    y:['Feb 23 09:03', 'Feb 23 09:04', 'Feb 23 09:05', 'Feb 23 09:06', 'Feb 23 09:07', 'Feb 23 09:08', 'Feb 23 09:09']
+        
+setTimeout(() => {
+    chart_tegangan.updateSeries([{
+        data:[{
+            x : 'Feb 23 09:04',
+            y : 385
+        },{
+            x : 'Feb 23 09:05',
+            y : 386
+        },{
+            x : 'Feb 23 09:06',
+            y : 384.6
+        },{
+            x : 'Feb 23 09:07',
+            y : 398.4
+        },{
+            x : 'Feb 23 09:08',
+            y : 385.2
+        },{
+            x : 'Feb 23 09:09',
+            y : 380.7
+        },{
+            x : 'Feb 23 09:10',
+            y : 398.2
+        },
+    ]
+    }]);
+}, 6000);
+
 
 </script>
--- resources/views/main/partials/kanan.blade.php
+++ resources/views/main/partials/kanan.blade.php
@@ -1,25 +1,272 @@
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Tegangan</span>
+    			<span class="text-muted fw-bold fs-7">Voltage</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_tegangan" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
 
-									<!--begin::Mixed Widget 5-->
-									<div class="card">
-										<!--begin::Beader-->
-										<div class="card-header border-0 py-5">
-											<h3 class="card-title align-items-start flex-column">
-												<span class="card-label fw-bolder fs-3 mb-1">Tegangan</span>
-												<span class="text-muted fw-bold fs-7">RMS (380V)</span>
-											</h3>
-											
-										</div>
-										<!--end::Header-->
-										<!--begin::Body-->
-										<div class="card-body d-flex flex-column">
-											<!--begin::Chart-->
-											<div id="chart_tegangan" style="height: 350px;"></div>
-											<!--end::Chart-->
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Arus</span>
+    			<span class="text-muted fw-bold fs-7">Current</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_arus" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
 
-										</div>
-										<!--end::Body-->
-									</div>
-									<!--end::Mixed Widget 5-->
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Faktor Daya</span>
+    			<span class="text-muted fw-bold fs-7">Power Factor</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_faktor_daya" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
 
-                
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Frekuensi</span>
+    			<span class="text-muted fw-bold fs-7">Frequency</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_frekuensi" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
 
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Daya Aktif</span>
+    			<span class="text-muted fw-bold fs-7">Active Power</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_daya_aktif" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Daya Reaktif</span>
+    			<span class="text-muted fw-bold fs-7">Reactive Power</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_daya_reaktif" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+        <!--begin::Mixed Widget 5-->
+        <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Daya Semu</span>
+    			<span class="text-muted fw-bold fs-7">PPP</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_daya_semu" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Total Daya Aktif</span>
+    			<span class="text-muted fw-bold fs-7">Active Power Total</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_total_daya_aktif" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Total Daya Reaktif</span>
+    			<span class="text-muted fw-bold fs-7">Reactive Power Total</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_total_daya_reaktif" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Total Daya Semu</span>
+    			<span class="text-muted fw-bold fs-7">ppp</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_total_daya_semu" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Total Faktor Daya</span>
+    			<span class="text-muted fw-bold fs-7">Total Power Factor</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_total_faktor_daya" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Daya Aktif yang diminta</span>
+    			<span class="text-muted fw-bold fs-7">Active Power Demand</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="daya_aktif_yang_diminta" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
+
+    <!--begin::Mixed Widget 5-->
+    <div class="card mb-5">
+    	<!--begin::Beader-->
+    	<div class="card-header border-0 py-5">
+    		<h3 class="card-title align-items-start flex-column">
+    			<span class="card-label fw-bolder fs-3 mb-1">Daya Reaktif yang diminta</span>
+    			<span class="text-muted fw-bold fs-7">Reactive Power Demand</span>
+    		</h3>
+    
+    	</div>
+    	<!--end::Header-->
+    	<!--begin::Body-->
+    	<div class="card-body d-flex flex-column">
+    		<!--begin::Chart-->
+    		<div id="chart_daya_reaktif_yang_diminta" style="height: 350px;"></div>
+    		<!--end::Chart-->
+    	</div>
+    	<!--end::Body-->
+    </div>
+    <!--end::Mixed Widget 5-->
