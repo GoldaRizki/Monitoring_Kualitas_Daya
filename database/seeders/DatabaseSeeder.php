3a2439df59ac0ab93d4929b81152e731bb66afa7
goldarizki <goldarizki@gmail.com>
2024-07-14 21:08:50 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\Data;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+
+        Data::create([
+            'tegangan' => 0,
+            'arus' => 0,
+            'faktor_daya' => 0,
+            'daya_aktif' => 0,
+            'daya_reaktif' => 0,
+            'daya_semu' => 0,
+            'total_daya_aktif' => 0,
+            'total_daya_reaktif' => 0,
+            'total_daya_semu' => 0,
+            'total_faktor_daya' => 0,
+            'frekuensi' => 0,
+            'reactive_power_demand' => 0,
+            'apparent_power_demand' => 0,
+        ]);
+
     }
 }
